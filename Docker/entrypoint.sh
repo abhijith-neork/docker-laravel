ace0794db20e0770680c14327a5edcd9276756f6
Ubuntu <ubuntu@ip-172-31-80-16.ec2.internal>
2024-02-16 07:03:14 +0000

    docker-files

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
+    composer install --ignore-platform-req=ext-gd
 fi
 
 if [ ! -f ".env" ]; then
@@ -15,16 +16,16 @@ role=${CONTAINER_ROLE:-app}
 
 if [ "$role" = "app" ]; then
     php artisan migrate
+    php artisan db:seed
     php artisan key:generate
     php artisan cache:clear
     php artisan config:clear
     php artisan route:clear
-    php artisan db:seed
     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint "$@"
 elif [ "$role" = "queue" ]; then
     echo "Running the queue ... "
-    php /var/www//html/artisan queue:work --verbose --tries=3 --timeout=180
+    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
 elif [ "$role" = "websocket" ]; then
     echo "Running the websocket server ... "
     php artisan websockets:serve
